c527e9c741b4216f01887567919b726e616e0d6a
xuanzhi33 <xuanzhi33@qq.com>
2024-02-04 21:31:27 +0800

    feat: init

--- smcl.py
+++ smcl.py
@@ -1,13 +1,31 @@
 import webview
+from subprocess import run as sp_run
+import sys
+CMCL = "./src/cmcl.exe"
+SRC_DIR = "./src/"
+MAIN = "main.html"
 
 class Api:
     def __init__(self, window) -> None:
         self.window = window
-    def test(self):
-        print('test')
-    def change_title(self, title):
+    def set_title(self, title):
         self.window.title = title
+    def log(self, message):
+        print(message)
+    def _run(self, args):
+        result = sp_run(args, capture_output=True, text=True)
+        return result.stdout
+    def cmcl(self, cmd):
+        args = cmd.split(" ")
+        return self._run([CMCL] + args)
 
-window = webview.create_window('Simple browser', './gui/main.html')
+window = webview.create_window('SMCL', SRC_DIR + MAIN)
 api = Api(window)
-webview.start(debug=True)
+window.expose(api.set_title, api.log, api.cmcl)
+
+if getattr(sys, 'frozen', False):
+    debug = False
+else:
+    debug = True
+
+webview.start(debug=debug)
