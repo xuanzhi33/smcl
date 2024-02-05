4fe98b0a43debdfa640d7bc4efb35348b2b5c92c
xuanzhi33 <xuanzhi33@qq.com>
2024-02-05 22:54:50 +0800

    feat: Refactor code structure and add settings functionality

---
+++ build.py
@@ -0,0 +1,14 @@
+import subprocess
+
+cmds = ["pyinstaller"]
+
+cmds.append("--onefile")
+# cmds.append("--windowed")
+cmds.append("--add-data")
+cmds.append("src:src")
+
+cmds.append("smcl.py")
+
+result = subprocess.run(cmds)
+
+print(result)
--- smcl.py
+++ smcl.py
@@ -1,12 +1,49 @@
 import webview
 from subprocess import run as sp_run
 import sys
+from os import path
+import json
+
 CMCL = "./src/cmcl.exe"
-SRC_DIR = "./src/"
-MAIN = "main.html"
+PATH = path.dirname(path.abspath(__file__))
+
+SRC_DIR = path.join(PATH, "src")
+GUI_MAIN = path.join(SRC_DIR, "main.html")
+SETTINGS = path.join(PATH, "smcl.json")
+SIZE = (800, 600)
+
+class Settings:
+    def __init__(self, file) -> None:
+        self.file = file
+        self.settings = {"test": "测试"}
+        self.load()
+    def init_file(self):
+        with open(self.file, "w") as f:
+            json.dump(self.settings, f)
+
+    def load(self):
+        if not path.exists(self.file):
+            self.init_file()
+               
+        with open(self.file, "r") as f:
+            self.settings = json.load(f)
+
+    def save(self):
+        with open(self.file, "w") as f:
+            json.dump(self.settings, f)
+    
+    def get(self, key):
+        return self.settings[key]
+    
+    def set(self, key, value):
+        self.settings[key] = value
+        self.save()
 
 class Api:
-    def __init__(self, window) -> None:
+    def __init__(self) -> None:
+        self.window = None
+        self.settings = Settings(SETTINGS)
+    def _set_window(self, window):
         self.window = window
     def set_title(self, title):
         self.window.title = title
@@ -18,14 +55,17 @@ class Api:
     def cmcl(self, cmd):
         args = cmd.split(" ")
         return self._run([CMCL] + args)
+    
+class UI:
+    def __init__(self) -> None:
+        self.api = Api()
+    def start(self):
+        window = webview.create_window('SMCL', GUI_MAIN, width=SIZE[0], height=SIZE[1], js_api=self.api)
+        self.api._set_window(window)
+    def isFrozen():
+        return hasattr(sys, "frozen")
+    def start(self):
+        webview.start(debug=self.isFrozen())
 
-window = webview.create_window('SMCL', SRC_DIR + MAIN)
-api = Api(window)
-window.expose(api.set_title, api.log, api.cmcl)
-
-if getattr(sys, 'frozen', False):
-    debug = False
-else:
-    debug = True
-
-webview.start(debug=debug)
+if __name__ == "__main__":
+    UI().start()
