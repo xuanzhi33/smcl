3c7ad17151e7161919ad1e1b212ced00c3c204e9
xuanzhi33 <xuanzhi33@qq.com>
2024-05-28 14:09:26 +0800

    feat: Launch game

--- smcl.py
+++ smcl.py
@@ -1,61 +1,74 @@
 import webview
-from subprocess import run as sp_run
+from subprocess import run as sp_run, PIPE, Popen
 import sys
-from os import path
+from os import path, remove, getcwd
 import json
 from webview import Window
 import requests
 from datetime import datetime
+from locale import getdefaultlocale
 PATH = path.dirname(path.abspath(__file__))
+CWD = getcwd()
 
 SRC_DIR = path.join(PATH, "src")
 GUI_MAIN = path.join(SRC_DIR, "main.html")
-SETTINGS = path.join(PATH, "smcl.json")
+CMCL_CONFIG = path.join(SRC_DIR, "cmcl.json")
 CMCL = path.join(SRC_DIR, "cmcl.exe")
 
 SIZE = (800, 450)
 DEBUG = True
 
 class Settings:
-    def __init__(self, file) -> None:
-        self.file = file
-        self.settings = {"test": "测试"}
-        self.load()
-    def init_file(self):
-        with open(self.file, "w") as f:
-            json.dump(self.settings, f)
-
-    def load(self):
-        if not path.exists(self.file):
-            self.init_file()
-               
-        with open(self.file, "r") as f:
-            self.settings = json.load(f)
-
-    def save(self):
-        with open(self.file, "w") as f:
-            json.dump(self.settings, f)
-    
     def get(self, key):
-        return self.settings[key]
-    
-    def set(self, key, value):
-        self.settings[key] = value
-        self.save()
+        with open(CMCL_CONFIG, "r", encoding="utf-8") as f:
+            data = json.load(f)
+            if key in data:
+                return data[key]
+            else:
+                return None
+    def set(self, settings):
+        with open(CMCL_CONFIG, "r", encoding="utf-8") as f:
+            data = json.load(f)
+            for key in settings:
+                data[key] = settings[key]
+        with open(CMCL_CONFIG, "w", encoding="utf-8") as f:
+            json.dump(data, f, indent=4)
 
 class Api:
     def __init__(self) -> None:
         self.window = None
-        self.settings = Settings(SETTINGS)
+        self.settings = Settings()
     def _set_window(self, window: Window):
         self.window = window
     def _run(self, args):
         result = sp_run(args, capture_output=True, text=True)
         return result.stdout
+    def run_js(self, js):
+        return self.window.evaluate_js(js)
+    def set_progress(self, progress):
+        self.run_js(f"window.setProgress({progress})")
+    def show_progress_info(self, info):
+        self.run_js(f"window.showProgressInfo(`{info}`)")
+    def cmcl_waiting(self, cmd):
+        self.log(f"CMCL: {cmd}")
+        args = [CMCL]
+        if cmd is not None:
+            args += cmd.split(" ")
+        
+        p = Popen(args, stdout=PIPE, stderr=PIPE, text=True)
+        prog = 0
+        for line in p.stdout:
+            print(line)
+            self.set_progress(prog)
+            self.show_progress_info(line)
+            prog += 1
+
     def set_title(self, title):
-        self.log(f"标题已经设置为: {title}")
+        self.log(f"New title: {title}")
         self.window.title = title
     def log(self, message):
+        if not DEBUG:
+            return
         time_str = datetime.now().strftime("%H:%M:%S")
         print(f"[SMCL] [{time_str}] {message}")
     def alert(self, message, title="SMCL"):
@@ -64,6 +77,11 @@ class Api:
         self.log(f"CMCL: {cmd}")
         args = cmd.split(" ")
         return self._run([CMCL] + args)
+    def config(self, key, value = None):
+        if value == None:
+            return self.settings.get(key)
+        else:
+            return self.settings.set({key: value})
     def close(self):
         self.window.destroy()
     def minimize(self):
@@ -75,18 +93,39 @@ class Api:
     def isDebug(self):
         return DEBUG
     def get(self, url):
-        self.log(f"GET请求: {url}")
+        self.log(f"GET: {url}")
         return requests.get(url).text
+    def cmcl_config_exists(self):
+        return path.exists(CMCL_CONFIG)
+    
+    def default_settings(self):
+        self.log("Initializing cmcl.json")
+        self.cmcl("config --clear")
+        lang = "en"
+        if getdefaultlocale()[0] == "zh_CN":
+            lang = "zh"
+        self.settings.set({
+            "downloadSource": 0,
+            "exitWithMinecraft": False,
+            "printStartupInfo": True,
+            "checkAccountBeforeStart": True,
+            "gameDir": path.join(CWD, ".minecraft"),
+            "language": lang
+        })
+
     def init_cmcl(self):
-        if not path.exists(path.join(SRC_DIR, "cmcl.json")):
-            self.log("未检测到cmcl.json, 正在初始化...")
-            self.cmcl("config downloadSource 1")
+        if not self.cmcl_config_exists():
+            self.log("cmcl.json not found, initializing...")
+            self.default_settings()
+            return True
+        else:
+            return False
 
 class UI:
     def __init__(self) -> None:
         self.api = Api()
     def start(self):
-        self.api.log(f"SMCL正在启动，DEBUG：{self.isDebug()}")
+        self.api.log(f"SMCL is starting, DEBUG: {self.isDebug()}")
         window = webview.create_window('SMCL', GUI_MAIN,
                                        width=SIZE[0], height=SIZE[1],
                                        js_api=self.api)
